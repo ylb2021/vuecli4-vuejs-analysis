5d902a5dc44eda95d6a9384256597a2d32cb159a
ylb2021 <yanlibo2021@163.com>
2020-04-22 15:29:33 +0800

    modified:   lib/vuejs/core/instance/index.js         modified:   package.json         modified:   src/App.vue         modified:   src/components/HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <input type="text" @change="msg=titile" v-model="titile" />
+    <HelloWorld :msg="msg" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+// ### There are eight lifecycle methods:
+
+// Before create
+// Created
+// Before mount
+// Mounted
+// Before update
+// Updated
+// Before destroy
+// Destroyed
 
 export default {
-  name: 'App',
+  name: "App",
+  data: function() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      titile: ""
+    };
+  },
   components: {
     HelloWorld
+  },
+  beforeCreate() {
+    console.log("beforCreate hook has been called");
+  },
+  created() {
+    console.log("Created hook has been called");
+  },
+  beforeMount() {
+    console.log("beforeMount is called");
+  },
+  mounted() {
+    console.log("mounted has been called");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate hook has been called");
+  },
+  updated() {
+    console.log("Updated hook has been called");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy hook has been called");
+  },
+  destroyed() {
+    this.$destroy();
+    console.log("destroyed", this);
   }
-}
+};
 </script>
 
 <style>
